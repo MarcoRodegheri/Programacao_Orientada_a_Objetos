adb19af5e59bc6b6d04ef049705d546f638a06a2
Marco Antônio De Carli Rodegheri <marcoantoniorodegheri@gmail.com>
2025-09-19 13:48:08 -0300

    Criei todas as pastas

---
+++ 07_Arquivos/src/main/java/edu/rodegheri/App.java
@@ -0,0 +1,7 @@
+package edu.rodegheri;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto 07_Arquivos criado com sucesso.");
+    }
+}
---
+++ 08_Serializacao/src/main/java/edu/rodegheri/App.java
@@ -0,0 +1,7 @@
+package edu.rodegheri;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto 08_Serializacao criado com sucesso.");
+    }
+}
