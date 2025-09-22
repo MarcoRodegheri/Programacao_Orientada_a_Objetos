665476d5c49877b0410096894535424797d10df7
Marco Antônio De Carli Rodegheri <marcoantoniorodegheri@gmail.com>
2025-09-22 12:48:09 -0300

    corrigido VariasEscalas

--- 00_MyFlight/src/main/java/edu/rodegheri/VooVariasEscalas.java
+++ 00_MyFlight/src/main/java/edu/rodegheri/VooVariasEscalas.java
@@ -4,25 +4,29 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class VooVariasEscalas extends Voo{
+public class VooVariasEscalas extends Voo {
 
-    private ArrayList<Voo> variasEscalas;
+    private ArrayList<Voo> variasEscalas = new ArrayList<>();;
 
     public VooVariasEscalas(LocalDateTime dataHora, Duration  duracao, Rota rota, ArrayList<Voo> listaVoo){
        super(dataHora, duracao, rota);
 
-        this.variasEscalas = new ArrayList<>();
+       this.variasEscalas.add(new Voo(getDataHora(), getDuracao(), getRota()));
+       this.variasEscalas.addAll(listaVoo);
+       setRota(new Rota(variasEscalas.get(0).getRota().getCiaAerea(), 
+                        variasEscalas.get(0).getRota().getOrigem(), 
+                        variasEscalas.get(variasEscalas.size()-1).getRota().getDestino(),
+                        variasEscalas.get(0).getRota().getAeronave()));
 
-        this.variasEscalas.add(new Voo(super.getDataHora(), super.getDuracao(), super.getRota()));
-        this.variasEscalas.addAll(listaVoo);
+          
     }
 
-    public ArrayList<Voo> getVariasEscalas(){
+    public ArrayList<Voo> getVariasEscalas() {
         return this.variasEscalas;
     }
 
     @Override
-    public String toString(){
-        return super.toString()+"Escalas: "+this.variasEscalas;
+    public String toString() {
+        return super.toString() + "Escalas: " + this.variasEscalas;
     }
 }
